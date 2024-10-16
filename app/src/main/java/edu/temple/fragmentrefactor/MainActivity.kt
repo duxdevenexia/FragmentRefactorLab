77d11beb882f84030effcc2c36940e27aa6014f4
duxdevenexia <88348139+duxdevenexia@users.noreply.github.com>
2024-10-16 15:43:30 -0400

    10/16/24 done

--- app/src/main/java/edu/temple/fragmentrefactor/MainActivity.kt
+++ app/src/main/java/edu/temple/fragmentrefactor/MainActivity.kt
@@ -11,9 +11,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        /*
+
+        // functionality: if nameEditText is blank,
+        // show "Please enter your name" in displayTextView
+        // else, show string of that textfield with greeting in displayTextView
+
+        // move these to MianFragment
         val displayTextView = findViewById<TextView>(R.id.displayTextView)
         val nameEditText = findViewById<EditText>(R.id.nameEditText)
         val changeButton = findViewById<Button>(R.id.changeButton)
+
 
         changeButton.setOnClickListener {
             val name = nameEditText.text
@@ -24,5 +32,21 @@ class MainActivity : AppCompatActivity() {
                 "Please enter your name"
             }
         }
+
+         */
+
+        // Add MainFragment to tthe activity
+        if (savedInstanceState == null) {
+            // create MainFragment object
+            // which performs the functionality performed by the original Activity
+            val mainFragment = MainFragment()
+
+            // supportFragmentManager and use transaction
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, mainFragment)
+                .commit()   // invoke commit
+        } // end
     }
 }
+
+
